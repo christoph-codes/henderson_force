8276e106c11a7ff42c17e4b40c8c9cfe263a02b7
christoph-codes <tkcwebdev@gmail.com>
2024-05-08 10:32:58 -0700

    main: Add sanity and tailwind merge config updates

--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -1,10 +1,27 @@
-import { createClient } from 'next-sanity'
+import { type QueryParams, createClient } from "next-sanity";
 
-import { apiVersion, dataset, projectId, useCdn } from '../env'
+import { apiVersion, dataset, projectId, useCdn } from "../env";
 
 export const client = createClient({
-  apiVersion,
-  dataset,
-  projectId,
-  useCdn,
-})
+	apiVersion,
+	dataset,
+	projectId,
+	useCdn,
+});
+
+export async function sanityFetch<QueryResponse>({
+	query,
+	params = {},
+	tags,
+}: {
+	query: string;
+	params?: QueryParams;
+	tags?: string[];
+}) {
+	return client.fetch<QueryResponse>(query, params, {
+		next: {
+			revalidate: process.env.NODE_ENV === "development" ? 30 : 3600,
+			tags,
+		},
+	});
+}
