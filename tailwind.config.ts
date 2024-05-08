2f7ab819eda36a258f6e95141184f54beb753a7b
christoph-codes <tkcwebdev@gmail.com>
2024-05-08 10:32:24 -0700

    main: Add brand fonts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,25 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+				bolts: "url('/bolts_bg.png')",
+			},
+			fontFamily: {
+				sans: ["var(--font-industry)"],
+				mono: ["var(--font-gothic)"],
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
