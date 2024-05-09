7ee11d832d306fc700df4b4b68e4ba09084005e8
christoph-codes <tkcwebdev@gmail.com>
2024-05-09 01:56:46 -0700

    20: Created join the force page. Resolves #20. Forms are setup with Resend

--- src/app/UnderConstruction.tsx
+++ src/app/UnderConstruction.tsx
@@ -1,14 +1,23 @@
+"use client";
+
+import { Button } from "@/components/Button";
 import { HForceVerticalLogo } from "@/components/HForceVerticalLogo";
 import { SanityDocument } from "next-sanity";
+import { useRouter } from "next/navigation";
 
 export function UnderConstruction({ content }: { content: SanityDocument }) {
-	console.log("content", content);
+	const router = useRouter();
 	return (
-		<div className="min-h-screen flex flex-col justify-center items-center bg-bolts bg-cover bg-no-repeat bg-center">
-			<div className="text-center flex flex-col items-center gap-8">
-				<HForceVerticalLogo className="max-h-72 md:max-h-100 mb-8" />
-				<h1 className="text-4xl md:text-6xl">{content.name}</h1>
-				<p>{content.description}</p>
+		<div className="min-h-screen flex flex-col md:justify-center pt-8 md:pt-0 items-center bg-bolts bg-cover bg-no-repeat bg-center">
+			<div className="text-center flex flex-col items-center gap-4 flex-wrap">
+				<HForceVerticalLogo className="max-h-72 md:max-h-[400px] mb-8" />
+				<h1 className="text-4xl md:text-6xl text-wrap">{content.name}</h1>
+				<p className="text-gray-300 text-xl">{content.description}</p>
+				<hr className="my-8 border-gray-500 border w-full inset-0" />
+				<h3 className="mb">Ready to play for the Force?</h3>
+				<Button onClick={() => router.push("/join")} className="mt-8">
+					Join The Force
+				</Button>
 			</div>
 		</div>
 	);
