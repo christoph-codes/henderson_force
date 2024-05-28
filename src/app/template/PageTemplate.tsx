a958342c7eace73e373cdba3b220d1d5fa6635eb
christoph-codes <tkcwebdev@gmail.com>
2024-05-28 00:18:48 -0700

    9: Add footer to page template

--- src/app/template/PageTemplate.tsx
+++ src/app/template/PageTemplate.tsx
@@ -2,6 +2,7 @@ import { Header } from "@/components/Header";
 import { SanityDocument } from "next-sanity";
 import { PropsWithChildren } from "react";
 import { sanityFetch } from "../../../sanity/lib/client";
+import Footer from "@/components/Footer";
 
 const GLOBAL_QUERY = `*[_type == "partner_logos"]`;
 
@@ -9,11 +10,12 @@ export async function PageTemplate({ children }: PropsWithChildren) {
 	const sponsors: SanityDocument[] = await sanityFetch<SanityDocument[]>({
 		query: GLOBAL_QUERY,
 	});
+
 	return (
 		<>
 			<Header sponsors={sponsors} />
-			<main className="py-8">{children}</main>
-			<footer>Footer</footer>
+			<main className="space-y-8">{children}</main>
+			<Footer />
 		</>
 	);
 }
