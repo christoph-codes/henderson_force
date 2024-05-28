013247c922c62772dcf74691c40101155c15e4ad
christoph-codes <tkcwebdev@gmail.com>
2024-05-27 17:46:50 -0700

    28: Show sample hero on the new homepage

--- src/app/home/Home.tsx
+++ src/app/home/Home.tsx
@@ -1,11 +1,12 @@
-import { PageTemplate } from "@/template/PageTemplate";
+import { Hero } from "@/components/Hero";
+import { PageTemplate } from "@/app/template/PageTemplate";
 import { SanityDocument } from "next-sanity";
 
 export function Home({ content }: { content: SanityDocument[] }) {
 	console.log("content", content);
 	return (
 		<PageTemplate>
-			<h1>Home</h1>
+			<Hero title="Home" description="Description of the home page" />
 		</PageTemplate>
 	);
 }
