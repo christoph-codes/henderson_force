e6773f0ca277301bc411bc754a22b3771195af77
christoph-codes <tkcwebdev@gmail.com>
2024-05-29 02:28:00 -0700

    34: Hide navigation links in footer for mobile since header is floating with scroll

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,30 +10,34 @@ const Footer = async () => {
 	});
 	return (
 		<footer className="py-8 text-center">
-			<div className="container text-center flex flex-col md:flex-row justify-center gap-6 mb-4">
-				{footerLinks.map((link: any) => {
-					return (
-						<Link
-							key={link._id}
-							href={link.link ?? ""}
-							target="_blank"
-							rel="noopener noreferrer"
-							className="text-white hover:text-primary transition-colors"
-						>
-							{link.name}
-						</Link>
-					);
-				})}
+			<div className="container">
+				{footerLinks && (
+					<div className="hidden text-center md:flex flex-col md:flex-row justify-center gap-6 mb-4">
+						{footerLinks.map((link: any) => {
+							return (
+								<Link
+									key={link._id}
+									href={link.link ?? ""}
+									target="_blank"
+									rel="noopener noreferrer"
+									className="text-white hover:text-primary transition-colors"
+								>
+									{link.name}
+								</Link>
+							);
+						})}
+					</div>
+				)}
+				<p className="text-gray-400">
+					Copyright © {new Date().getFullYear()}. Henderson Force. All Rights
+					Reserved.
+				</p>
+				<img
+					src="/hforce_icon.svg"
+					alt="Henderson Force Icon"
+					className="w-16 h-16 mx-auto"
+				/>
 			</div>
-			<p className="text-gray-400">
-				Copyright © {new Date().getFullYear()}. Henderson Force. All Rights
-				Reserved.
-			</p>
-			<img
-				src="/hforce_icon.svg"
-				alt="Henderson Force Icon"
-				className="w-16 h-16 mx-auto"
-			/>
 		</footer>
 	);
 };
