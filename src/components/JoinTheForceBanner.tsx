a15aa6f42981718a225058832c613b0f17dfa2d0
christoph-codes <tkcwebdev@gmail.com>
2024-05-29 01:13:55 -0700

    34: Add hover effect for attention

--- src/components/JoinTheForceBanner.tsx
+++ src/components/JoinTheForceBanner.tsx
@@ -2,20 +2,21 @@ import Link from "next/link";
 
 const JoinTheForceBanner = () => {
 	return (
-		<Link className="block" href="/join">
-			<section className="container gap-4 flex-col md:flex-row bg-[url('/join_force_bg.png')] bg-cover bg-no-repeat bg-full flex justify-around items-center p-8">
+		<Link className="block group" href="/join">
+			<section className="container group-hover:opacity-100 transition-opacity opacity-80 gap-4 flex-col md:flex-row bg-[url('/join_force_bg.png')] bg-cover bg-no-repeat bg-full flex justify-around items-center p-8">
 				<img
 					src="/hforce_icon_alt.svg"
 					alt="Henderson Force Icon"
 					className="w-20 h-20 md:w-36 md:h-36"
 				/>
-				<h2 className="font-sans text-[72px] md:text-[90px] leading-tight text-center">
+				<h2 className="font-sans text-[72px] md:text-[90px] leading-[72px] md:leading-[90px] text-center mb-0r">
 					Join the <span className="underline">Force</span>
+					<span className="text-2xl block mt-4">(Player Interest Form)</span>
 				</h2>
 				<img
 					src="/hforce_icon_alt.svg"
 					alt="Henderson Force Icon"
-					className="w-20 h-20 md:w-36 md:h-36"
+					className="w-20 h-20 md:w-36 md:h-36 hidden md:block"
 				/>
 			</section>
 		</Link>
