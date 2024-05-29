0484d553d444d699f53f7824cb479210f873b9ce
christoph-codes <tkcwebdev@gmail.com>
2024-05-29 12:15:51 -0700

    main: Create social page to redirect to when finishing a form

---
+++ src/app/social/page.tsx
@@ -0,0 +1,28 @@
+import { SanityDocument } from "next-sanity";
+import { Metadata } from "next";
+import { PageTemplate } from "@/app/template/PageTemplate";
+import { querySanity, sanityFetch } from "../../../sanity/lib/client";
+import Social from "./Social";
+
+const PAGE_QUERY = `*[_type == "page" && slug.current == "social"]`;
+
+export async function generateMetadata(): Promise<Metadata> {
+	const [content] = await querySanity<SanityDocument[]>(PAGE_QUERY);
+
+	return {
+		title: `${content.name} | Henderson Force`,
+		description: content.description,
+	};
+}
+
+export default async function Page() {
+	const content: SanityDocument = await sanityFetch<SanityDocument>({
+		query: PAGE_QUERY,
+	});
+
+	return (
+		<PageTemplate content={content[0]?.page_content}>
+			<Social content={content[0]} />
+		</PageTemplate>
+	);
+}
