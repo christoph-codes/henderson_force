16867f087cd844e6fec8eacb53c1613826389bad
christoph-codes <tkcwebdev@gmail.com>
2024-05-28 23:49:34 -0700

    34: highlight current active page route

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,10 @@
 import { SanityDocument } from "next-sanity";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { useState } from "react";
 
 export function Navigation({ navItems }: { navItems: SanityDocument[] }) {
+	const pathname = usePathname();
 	const [hoveredDropdown, setHoveredDropdown] = useState<string | null>(null);
 
 	const handleMouseEnter = (itemId: string) => {
@@ -19,6 +21,9 @@ export function Navigation({ navItems }: { navItems: SanityDocument[] }) {
 				const hasChildren = navItem.children && navItem.children.length > 0;
 				const isDropdownOpen = hoveredDropdown === navItem._id;
 
+				const isNavItemActive =
+					pathname.split("/")[1] === navItem.link.split("/")[1];
+
 				return (
 					<button
 						key={navItem._id}
@@ -26,7 +31,12 @@ export function Navigation({ navItems }: { navItems: SanityDocument[] }) {
 						onMouseEnter={() => handleMouseEnter(navItem._id)}
 						onMouseLeave={handleMouseLeave}
 					>
-						{navItem.name}
+						<Link
+							className={`${isNavItemActive ? "font-bold text-white" : ""}`}
+							href={navItem.link}
+						>
+							{navItem.name}
+						</Link>
 
 						{hasChildren && isDropdownOpen && (
 							<div className="bg-gray-900 flex-col gap-2 rounded-md absolute p-3 flex">
