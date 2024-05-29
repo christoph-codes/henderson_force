724797649a1dd92c88fab083cd027abf66ea6ee4
Christopher Jones <tkcwebdev@gmail.com>
2024-05-28 19:27:25 -0700

    34: Style banner card for mobile

--- src/components/BannerCard.tsx
+++ src/components/BannerCard.tsx
@@ -11,9 +11,11 @@ const BannerCard = ({ title, link, image }: BannerCardProps) => {
 		<Link href={link} className="">
 			<div
 				style={image ? { backgroundImage: `url('${image}')` } : {}}
-				className={`bg-gray-900 h-[250px] md:h-[350px] flex justify-end items-center rounded-lg`}
+				className={`bg-gray-900 h-[250px] md:h-[350px] flex justify-end items-center rounded-lg p-3`}
 			>
-				<h2 className="w-1/3 text-[42px]">{title}</h2>
+				<h2 className="w-full md:w-1/3 text-[42px] text-center md:text-left">
+					{title}
+				</h2>
 			</div>
 		</Link>
 	);
