c191af44ba7cc4295ee45775b9bb3129fb215127
christoph-codes <tkcwebdev@gmail.com>
2024-06-06 11:26:46 -0700

    39: Update flex card to show hover state with the group hover versus them being separate

--- src/components/FlexCard.tsx
+++ src/components/FlexCard.tsx
@@ -11,7 +11,7 @@ const FlexCard = ({ link, image, title, description }: FlexCardProps) => {
 	return (
 		<Link href={link} className="group">
 			<div
-				className="bg-cover shadow-lg hover:shadow-2xl transition-all hover:scale-110 mb-3 justify-center h-40 items-center bg-black bg-center p-8 flex flex-col gap-4 text-white text-center font-sans text-xl"
+				className="bg-cover shadow-lg group-hover:shadow-2xl transition-all group-hover:scale-110 mb-3 justify-center h-40 items-center bg-black bg-center p-8 flex flex-col gap-4 text-white text-center font-sans text-xl"
 				style={
 					image
 						? {
