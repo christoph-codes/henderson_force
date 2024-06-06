8727e9096fe21295c20afe7f4e3e8c67473b635b
christoph-codes <tkcwebdev@gmail.com>
2024-06-06 13:32:28 -0700

    main: Allow sorting to come through automatically since you can reorder in the gui. Fixes flex card order with billet info being last

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,11 +1,11 @@
 import { SanityDocument } from "next-sanity";
 import { Home } from "./Home";
 import { Metadata } from "next";
-import { querySanity, sanityFetch } from "../../../sanity/lib/client";
+import { querySanity } from "../../../sanity/lib/client";
 import { PageTemplate } from "../template/PageTemplate";
 
 const PAGE_QUERY = `*[_type == "page" && slug.current == "home"]`;
-const FLEX_CARDS_QUERY = `*[_type == "siteConfig"] | order(order asc)`;
+const FLEX_CARDS_QUERY = `*[_type == "siteConfig"]`;
 
 export async function generateMetadata(): Promise<Metadata> {
 	const [content] = await querySanity<SanityDocument[]>(PAGE_QUERY);
