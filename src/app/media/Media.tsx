53583d31cbe0b7b279174103784cbfadc123c0a1
christoph-codes <tkcwebdev@gmail.com>
2024-06-13 19:09:46 -0700

    46: Add instagram feed to new media page and update routes to go to this page instead of instagram

--- src/app/media/Media.tsx
+++ src/app/media/Media.tsx
@@ -1,13 +1,14 @@
 import { Hero } from "@/components/Hero";
 import { SanityDocument } from "next-sanity";
-import { urlForImage } from "../../../sanity/lib/image";
+import InstagramFeed from "@/components/InstagramFeed";
+import { InstagramPostData } from "./page";
 
 const Media = ({
 	content,
-	media,
+	instagramPosts,
 }: {
 	content: SanityDocument;
-	media: SanityDocument[];
+	instagramPosts: InstagramPostData[];
 }) => {
 	return (
 		<>
@@ -17,17 +18,7 @@ const Media = ({
 				description={content.description}
 				logo
 			/>
-			<section className="container text-center">
-				<div className="grid grid-rows-3 md:grid-rows-1 grid-cols-1 md:grid-cols-3 grid-flow-col auto-rows-auto gap-8 md:gap-6 container">
-					{media.map((post) => (
-						<img
-							src={post.image && urlForImage(post.image)}
-							alt={post.title}
-							key={post._id}
-						/>
-					))}
-				</div>
-			</section>
+			<InstagramFeed posts={instagramPosts} />
 		</>
 	);
 };
