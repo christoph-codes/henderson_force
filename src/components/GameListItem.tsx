ad5eb687061080ebb421bf41387ad8b2b26c5742
christoph-codes <tkcwebdev@gmail.com>
2024-08-06 00:06:05 -0700

    56: Create game list item copmonent for calendar page

---
+++ src/components/GameListItem.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { formatTimestampToDate } from "@/utils/helpers";
+import Link from "next/link";
+import { urlForImage } from "../../sanity/lib/image";
+
+export type GameListItemProps = {
+	image: string;
+	title: string;
+	venue: string;
+	atvs: "at" | "vs";
+	date: string;
+	link: string;
+};
+
+const GameListItem = ({
+	image,
+	title,
+	date,
+	venue,
+	atvs,
+	link,
+}: GameListItemProps) => {
+	return (
+		<tr className="rounded-md border-b border-gray-800 text-gray-400">
+			<td className="hidden md:table-cell p-2">
+				<img
+					src={image ?? "/hforce_icon.svg"}
+					alt={`${title} image`}
+					className="w-10 h-10 min-w-10 min-h-10 bg-black rounded-lg p-1"
+				/>
+			</td>
+			<td className="p-2 font-bold text-left">
+				{atvs} {title}
+			</td>
+			<td className="p-2 font-bold text-left">{venue}</td>
+			<td className="p-2 font-bold text-left">{formatTimestampToDate(date)}</td>
+			<td className="p-2 text-right">
+				<Link
+					className="text-white hover:text-primary font-bold transition-colors"
+					href={link}
+				>
+					Game Link
+				</Link>
+			</td>
+		</tr>
+	);
+};
+
+export default GameListItem;
