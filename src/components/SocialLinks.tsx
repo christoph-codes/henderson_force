31778e4738343204483688ee0d155f749ba91a2d
christoph-codes <tkcwebdev@gmail.com>
2024-09-03 22:55:38 -0700

    60: Add confirmation for submitting billet info and fetch social links on the client. Resolves #60

--- src/components/SocialLinks.tsx
+++ src/components/SocialLinks.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { SanityDocument } from "next-sanity";
 import { querySanity } from "../../sanity/lib/client";
 import Link from "next/link";
@@ -5,13 +7,25 @@ import { getSocialIcons } from "@/utils/helpers";
 
 const SOCIAL_LINKS_QUERY = `*[_type == "link" && "social" in category[]]`;
 
-const SocialLinks = async ({ size = 8 }) => {
-	const socialLinks: SanityDocument[] =
-		await querySanity<SanityDocument[]>(SOCIAL_LINKS_QUERY);
+import { useEffect, useState } from "react";
+
+const SocialLinks = ({ size = 8 }) => {
+	const [links, setLinks] = useState<SanityDocument[]>([]);
+
+	useEffect(() => {
+		const fetchSocialLinks = async () => {
+			const fetchedLinks: SanityDocument[] =
+				await querySanity<SanityDocument[]>(SOCIAL_LINKS_QUERY);
+			setLinks(fetchedLinks);
+		};
+
+		fetchSocialLinks();
+	}, []);
+
 	return (
-		socialLinks.length > 0 && (
+		links.length > 0 && (
 			<div className="flex justify-center gap-4 md:flex-row md:gap-8 mb-4">
-				{socialLinks.map((link) => {
+				{links.map((link) => {
 					return (
 						<Link
 							key={link._id}
