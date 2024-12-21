e165bdf13fe7ba498c6b0c705fb1362b680fd942
christoph-codes <tkcwebdev@gmail.com>
2024-12-21 11:44:18 -0800

    62: Use embed code for instagram feed instead of the deprecated instagram display api. Resolves #62

--- src/app/media/Media.tsx
+++ src/app/media/Media.tsx
@@ -1,26 +1,22 @@
 import { Hero } from "@/components/Hero";
 import { SanityDocument } from "next-sanity";
-import InstagramFeed from "@/components/InstagramFeed";
-import { InstagramPostData } from "./page";
+import InstagramEmbed from "@/components/InstagramEmbed";
 
-const Media = ({
-  content,
-  instagramPosts,
-}: {
-  content: SanityDocument;
-  instagramPosts: InstagramPostData[];
-}) => {
-  return (
-    <>
-      <Hero
-        className="bg-[url('/default_bg.png')]"
-        title={content.name}
-        description={content.description}
-        logo
-      />
-      <InstagramFeed posts={instagramPosts} />
-    </>
-  );
+const Media = ({ content }: { content: SanityDocument }) => {
+	return (
+		<>
+			<Hero
+				className="bg-[url('/default_bg.png')]"
+				title={content.name}
+				description={content.description}
+				logo
+			/>
+			{/* <InstagramFeed posts={instagramPosts} /> */}
+			<div className="container text-center py-12">
+				<InstagramEmbed />
+			</div>
+		</>
+	);
 };
 
 export default Media;
--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -15,18 +15,6 @@ export type InstagramPostData = {
 
 const PAGE_QUERY = `*[_type == "page" && slug.current == "media"]`;
 
-const instagramUrl = `https://graph.instagram.com/${process.env.NEXT_PUBLIC_INSTAGRAM_USERID}/media?fields=id,media_type,media_url,permalink,caption,thumbnail_url&access_token=${process.env.NEXT_PUBLIC_INSTAGRAM_ACCESSTOKEN_LONGLIVE}`;
-
-async function getInstagramPosts() {
-	const res = await fetch(instagramUrl, { method: "GET" });
-	// The return value is *not* serialized
-	if (!res.ok) {
-		// This will activate the closest `error.js` Error Boundary
-		throw new Error("Failed to fetch instagram posts");
-	}
-	return res.json() as Promise<{ data: InstagramPostData[] }>;
-}
-
 export async function generateMetadata(): Promise<Metadata> {
 	const [content] = await querySanity<SanityDocument[]>(PAGE_QUERY);
 
@@ -40,15 +28,10 @@ export default async function Page() {
 	const content = await sanityFetch<SanityDocument>({
 		query: PAGE_QUERY,
 	});
-	const instagramPosts = await getInstagramPosts();
-	// console.log("instagramPosts", instagramPosts);
 
 	return (
 		<PageTemplate content={content[0]?.page_content}>
-			<Media
-				content={content[0]}
-				instagramPosts={instagramPosts.data.slice(0, 24)}
-			/>
+			<Media content={content[0]} />
 		</PageTemplate>
 	);
 }
