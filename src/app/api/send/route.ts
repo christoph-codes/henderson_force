1205fa384d99eab6aafe9b11b26b630866a735d9
christoph-codes <tkcwebdev@gmail.com>
2025-03-21 14:47:38 -0700

    66: Add ability to add emails to sanity backend that will be notified with any form submission

--- src/app/api/send/route.ts
+++ src/app/api/send/route.ts
@@ -1,32 +1,31 @@
 import DefaultEmail from "@/emails/defaultEmail";
-import {
-  blindRecipients,
-  primaryEmailRecipients,
-} from "@/utils/emailRecipients";
 import { Resend } from "resend";
+import { querySanity } from "../../../../sanity/lib/client";
+import { SanityDocument } from "next-sanity";
 
 const resend = new Resend(process.env.NEXT_PUBLIC_RESEND_API);
 
-export async function POST(req: Request) {
-  const body = await req.json();
-  try {
-    const data = await resend.emails.send({
-      from: "Henderson Force Admin <info@hendersonforce.com>",
-      to:
-        process.env.NODE_ENV === "production"
-          ? body.to
-            ? [body.to]
-            : primaryEmailRecipients
-          : ["tkcwebdev@gmail.com"],
-      bcc: process.env.NODE_ENV === "production" ? blindRecipients : [],
-      subject: body.subject,
-      react: DefaultEmail(body.fields, body.subject),
-      text: body.subject,
-    });
+const SITE_CONFIG_QUERY = `*[_type == "siteConfig"]`;
 
-    return Response.json(data);
-  } catch (error) {
-    console.log("error", error);
-    return Response.json({ error });
-  }
+export async function POST(req: Request) {
+	const body = await req.json();
+	const query = await querySanity<SanityDocument>(SITE_CONFIG_QUERY);
+	const siteConfig = query[0] as SanityDocument;
+	const contactEmails = siteConfig.contactEmails as string[];
+	const devEmails = siteConfig.devEmails as string[];
+
+	try {
+		const data = await resend.emails.send({
+			from: "Henderson Force Admin <info@hendersonforce.com>",
+			to: process.env.NODE_ENV === "production" ? contactEmails : devEmails,
+			subject: body.subject,
+			react: DefaultEmail(body.fields, body.subject),
+			text: body.subject,
+		});
+
+		return Response.json(data);
+	} catch (error) {
+		console.log("error", error);
+		return Response.json({ error });
+	}
 }
